cdef1a8d3b1afde6883c5951a3939b336f38a1e8
sy-records <52o@qq52o.cn>
2020-01-11 08:23:52 +0800

    optimization pre_comment_approved

--- wp-baidu-textcensor.php
+++ wp-baidu-textcensor.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name:  Baidu TextCensor
 Plugin URI:   https://github.com/sy-records/wp-baidu-textcensor
-Description:  在WordPress中加入百度AI文本内容审核，过滤敏感内容
-Version:      1.0.0
+Description:  在WordPress中加入百度文本内容审核，过滤评论中的敏感内容
+Version:      1.0.1
 Author:       沈唁
 Author URI:   https://qq52o.me
 License:      Apache 2.0
@@ -112,11 +112,11 @@ function bdtc_submit_options()
     echo '</tr>';
     echo '<tr valign="top">';
     echo '<th scope="row">是否过滤博主</th>';
-    echo '<td><label><input value="true" type="checkbox" name="check_me" ' . $check_me . '> 勾选后会跳过博主评论内容，不去验证。</label></td>';
+    echo '<td><label><input value="true" type="checkbox" name="check_me" ' . $check_me . '> 勾选后会跳过博主评论内容（需要是登录状态），不去验证。</label></td>';
     echo '</tr>';
     echo '<tr valign="top">';
     echo '<th scope="row">是否删除配置信息</th>';
-    echo '<td><label><input value="true" type="checkbox" name="delete" ' . $delete . '> 勾选后停用插件会删除保存的配置信息，减少数据库垃圾数据！</label></td>';
+    echo '<td><label><input value="true" type="checkbox" name="delete" ' . $delete . '> 勾选后删除插件时会删除保存的配置信息，减少数据库垃圾数据！</label></td>';
     echo '</tr>';
     echo '</table>';
     echo '<p class="submit">';
@@ -124,9 +124,9 @@ function bdtc_submit_options()
     echo '</p>';
     echo '</form>';
     echo '<p><strong>使用提示</strong>：<br>
-	1. AppID、API Key、Secret Key在百度Ai控制台的 <a target="_blank" href="https://console.bce.baidu.com/ai/?fromai=1#/ai/antiporn/app/list">产品服务 / 内容审核 - 应用列表</a> 创建应用后获取；<br>
+	1. AppID、API Key、Secret Key在百度 AI 控制台的 <a target="_blank" href="https://console.bce.baidu.com/ai/?fromai=1#/ai/antiporn/app/list">产品服务 / 内容审核 - 应用列表</a> 创建应用后获取；<br>
 	2. 百度有默认审核策略，如果误杀严重，请进入 <a target="_blank" href="https://ai.baidu.com/censoring#/strategylist">内容审核平台创建自定义规则</a> 进行修改策略；<br>
-	3. 如有问题请至 <a target="_blank" href="https://github.com/sy-records/wp-baidu-textcensor">Github</a> 查看使用说明或至沈唁志博客 <a target="_blank" href="https://qq52o.me/guestbook.html">留言簿</a> 留言反馈。<br>
+	3. 如有问题请至 <a target="_blank" href="https://github.com/sy-records/wp-baidu-textcensor">Github</a> 查看使用说明或至沈唁志博客 <a target="_blank" href="https://qq52o.me/2720.html">插件发布页</a> 留言反馈。<br>
 	</p>';
     echo '</div>';
 }
@@ -149,13 +149,6 @@ if (!function_exists('is_user_logged_in')) {
     require(ABSPATH . WPINC . '/pluggable.php');
 }
 
-// change comment status
-function bdtc_3_4_comment_to_pending( $approved , $commentdata )
-{
-    $approved = 0;
-    return $approved;
-}
-
 // check comment
 function bdtc_refused_comments($comment_data)
 {
@@ -169,7 +162,7 @@ function bdtc_refused_comments($comment_data)
             wp_die("评论内容" . $res['data'][0]['msg'] . "，请重新评论");
         } elseif (in_array($res['conclusionType'], [3, 4])) {
             // 疑似和失败的写数据库，人工审核
-            add_filter( 'pre_comment_approved' , 'bdtc_3_4_comment_to_pending' , '99', 2 );
+            add_filter( 'pre_comment_approved' , '__return_zero');
         }
     }
     return( $comment_data );
